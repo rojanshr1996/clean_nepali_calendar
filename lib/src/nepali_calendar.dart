930b30dba44111d1c9bbfb40be33e87b40da9104
Rojan Shrestha <roj.shr1996@gmail.com>
2023-05-18 17:17:17 +0545

    Performance improved by replacing setStates with valuenotifiers in the main picker view

--- lib/src/nepali_calendar.dart
+++ lib/src/nepali_calendar.dart
@@ -15,7 +15,7 @@ const int _kMaxDayPickerRowCount = 6; // A 31 day month that starts on Saturday.
 // Two extra rows: one for the day-of-week header and one for the month header.
 const double _kMaxDayPickerHeight = _kDayPickerRowHeight * (_kMaxDayPickerRowCount + 2);
 
-class CleanNepaliCalendar extends StatefulWidget {
+class CleanNepaliCalendar<T> extends StatefulWidget {
   const CleanNepaliCalendar({
     Key? key,
     this.initialDate,
@@ -34,6 +34,8 @@ class CleanNepaliCalendar extends StatefulWidget {
     this.dateCellBuilder,
     this.enableVibration = true,
     this.headerBuilder,
+    this.eventMarkerBuilder,
+    this.eventLoader,
   }) : super(key: key);
 
   final NepaliDateTime? initialDate;
@@ -51,17 +53,22 @@ class CleanNepaliCalendar extends StatefulWidget {
   final HeaderDayBuilder? headerDayBuilder;
   final DateCellBuilder? dateCellBuilder;
   final HeaderBuilder? headerBuilder;
+  final EventMarkerBuilder? eventMarkerBuilder;
   final bool enableVibration;
 
+  /// Function that assigns a list of events to a specified day.
+  final List<T> Function(NepaliDateTime day)? eventLoader;
+
   @override
-  CleanNepaliCalendarState createState() => CleanNepaliCalendarState();
+  CleanNepaliCalendarState<T> createState() => CleanNepaliCalendarState<T>();
 }
 
-class CleanNepaliCalendarState extends State<CleanNepaliCalendar> {
+class CleanNepaliCalendarState<T> extends State<CleanNepaliCalendar<T>> {
+  late ValueNotifier<NepaliDateTime> _selectedDate;
   @override
   void initState() {
     super.initState();
-    _selectedDate = widget.initialDate ?? NepaliDateTime.now();
+    _selectedDate = ValueNotifier<NepaliDateTime>(widget.initialDate ?? NepaliDateTime.now());
     widget.controller._init(
       selectedDayCallback: _handleDayChanged,
       initialDay: widget.initialDate ?? NepaliDateTime.now(),
@@ -81,20 +88,19 @@ class CleanNepaliCalendarState extends State<CleanNepaliCalendar> {
     if (!_announcedInitialDate) {
       _announcedInitialDate = true;
       SemanticsService.announce(
-        NepaliDateFormat.yMMMMd().format(_selectedDate),
+        NepaliDateFormat.yMMMMd().format(_selectedDate.value),
         textDirection,
       );
     }
   }
 
   @override
-  void didUpdateWidget(CleanNepaliCalendar oldWidget) {
+  void didUpdateWidget(CleanNepaliCalendar<T> oldWidget) {
     super.didUpdateWidget(oldWidget);
-    _selectedDate = widget.initialDate ?? NepaliDateTime.now();
+    _selectedDate.value = widget.initialDate ?? NepaliDateTime.now();
     widget.controller.setSelectedDay(widget.initialDate ?? NepaliDateTime.now());
   }
 
-  late NepaliDateTime _selectedDate;
   final GlobalKey _pickerKey = GlobalKey();
 
   void _vibrate() {
@@ -103,34 +109,40 @@ class CleanNepaliCalendarState extends State<CleanNepaliCalendar> {
 
   void _handleDayChanged(NepaliDateTime value, {bool runCallback = true}) {
     if (widget.enableVibration) _vibrate();
-    setState(() {
-      widget.controller.setSelectedDay(value, isProgrammatic: false);
-      _selectedDate = value;
-    });
+
+    widget.controller.setSelectedDay(value, isProgrammatic: false);
+    _selectedDate.value = value;
+
     if (runCallback && widget.onDaySelected != null) {
       widget.onDaySelected!(value);
     }
   }
 
   Widget _buildPicker() {
-    return _MonthView(
-      key: _pickerKey,
-      headerStyle: widget.headerStyle,
-      calendarStyle: widget.calendarStyle,
-      language: widget.language,
-      selectedDate: _selectedDate,
-      onChanged: _handleDayChanged,
-      firstDate: widget.firstDate ?? NepaliDateTime(2000, 1),
-      lastDate: widget.lastDate ?? NepaliDateTime(2095, 12),
-      selectableDayPredicate: widget.selectableDayPredicate,
-      onHeaderTapped: widget.onHeaderTapped,
-      onHeaderLongPressed: widget.onHeaderLongPressed,
-      headerDayType: widget.headerDayType,
-      headerDayBuilder: widget.headerDayBuilder,
-      dateCellBuilder: widget.dateCellBuilder,
-      headerBuilder: widget.headerBuilder,
-      dragStartBehavior: DragStartBehavior.start,
-    );
+    return ValueListenableBuilder(
+        valueListenable: _selectedDate,
+        builder: (BuildContext context, NepaliDateTime value, Widget? child) {
+          return _MonthView(
+            key: _pickerKey,
+            headerStyle: widget.headerStyle,
+            calendarStyle: widget.calendarStyle,
+            language: widget.language,
+            selectedDate: _selectedDate.value,
+            onChanged: _handleDayChanged,
+            firstDate: widget.firstDate ?? NepaliDateTime(2000, 1),
+            lastDate: widget.lastDate ?? NepaliDateTime(2095, 12),
+            selectableDayPredicate: widget.selectableDayPredicate,
+            onHeaderTapped: widget.onHeaderTapped,
+            onHeaderLongPressed: widget.onHeaderLongPressed,
+            headerDayType: widget.headerDayType,
+            headerDayBuilder: widget.headerDayBuilder,
+            dateCellBuilder: widget.dateCellBuilder,
+            headerBuilder: widget.headerBuilder,
+            dragStartBehavior: DragStartBehavior.start,
+            eventLoader: widget.eventLoader,
+            eventMarkerBuilder: widget.eventMarkerBuilder,
+          );
+        });
   }
 
   @override
